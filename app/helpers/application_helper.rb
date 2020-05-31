68aab98a251f513f9dcdc9ee42a136c239a1ec55
Christian Pahl <ch.pahl@muench-its.de>
2020-05-31 17:53:56 +0200

    bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
     if user.avatar.attached?
       user.avatar.variant(resize: "#{size}x#{size}!")
     else
-      gravatar_image_url(user.email, size: size)
+      gravatar_image_url(user.username)
     end
   end
 end
