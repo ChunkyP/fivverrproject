2883a840391ae0eafd3e12e0b204d880ac4ed430
Christian Pahl <ch.pahl@muench-its.de>
2020-07-10 18:37:20 +0200

    betreiber fix

--- test/controllers/nachrichtens_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NachrichtensControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get nachrichtens_index_url
-    assert_response :success
-  end
-
-end
