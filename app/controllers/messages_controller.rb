fd91a08d7fcf716a27c26b574c2b21d9b43c9e68
Chris <christian1pahl@web.de>
2020-07-16 15:37:29 +0200

    limit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
     unless channels_ids.include? params[:channel_id].to_i
       redirect_to root_path, alert: 'Zugriff verweigert.'
     end
-    @messages = @channel.messages.order('created_at asc').limit(50).reverse
+    @messages = @channel.messages.last(50).order('created_at asc')
     @message = Message.new
     @channels = current_user.channels
     channel_user = ChannelsUsers.where(user_id: current_user.id, channel_id: params[:channel_id])
